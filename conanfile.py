2ced4fab8de8b2909c340ce68dfa0dc37b49442c
John Freeman <jfreeman08@gmail.com>
2019-06-12 12:04:39 -0500

    Hard code recipe attributes because toml unavailable

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,17 @@
+from conans import ConanFile
+
 from conan_cmake import CMakeConanFile as _CMakeConanFile
-from conan_cmake.python_conanfile import PythonConanFile
 
 
-class Recipe(PythonConanFile):
-    name = PythonConanFile.name
-    version = PythonConanFile.version
+class Recipe(ConanFile):
+    name = 'conan_cmake'
+    version = '0.1.0'
+    description = 'A generic Conan recipe for CMake projects.'
+    author = 'John Freeman <jfreeman08@gmail.com>'
+    homepage = 'https://conan-cmake.readthedocs.io/'
+    url = 'https://github.com/thejohnfreeman/conan-cmake/'
+    license = 'ISC'
+    exports = 'conan_cmake/**.py'
 
 
 def base():
