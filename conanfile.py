2b32d7e65c7b23e830281cd00cd25872b70189e0
John Freeman <jfreeman08@gmail.com>
2019-07-02 13:56:36 -0500

    Bump version to 0.3.0

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from autorecipes.descriptors import cached_classproperty
 
 class Recipe(ConanFile):
     name = 'autorecipes'
-    version = '0.2.0'
+    version = '0.3.0'
     description = 'Generic Conan recipes for CMake and Python projects.'
     author = 'John Freeman <jfreeman08@gmail.com>'
     homepage = 'https://autorecipes.readthedocs.io/'
